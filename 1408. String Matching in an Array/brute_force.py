368fb9d9cdc61a4415851b5626905550b2bbd3f7
husamXA1 <husamxm0@gmail.com>
2025-01-07 03:43:50 +0200

    1408 brute force solution

---
+++ 1408. String Matching in an Array/brute_force.py
@@ -0,0 +1,10 @@
+def stringMatching(words):
+    result = []
+    for i in range(len(words)):
+        for j in range(len(words)):
+            if i == j:
+                continue
+            if words[i] in words[j]:
+                result.append(words[i])
+                break
+    return result
